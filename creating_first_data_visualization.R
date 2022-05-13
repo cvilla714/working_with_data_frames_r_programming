bd9941b9082de85014be9d3a640f6cacf28a5825
Cosmel VG <cosmelvg@gmail.com>
2022-05-13 15:24:26 +0000

    Adding new examples
    - how to add different attributes to the ggplot
      - color
      - shape
      - size
      - alpha
      - and combine them

--- creating_first_data_visualization.R
+++ creating_first_data_visualization.R
@@ -13,4 +13,19 @@ data("penguins")
 View(penguins)
 
 #create the data visualization by using ggplot2
-ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+#ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+#improving the scatter plot by adding color the species
+#ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,color=species))
+
+#adding shape and color to make it look better
+#ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,color=species,shape=species))
+
+#adding size to it as well besides color and shape
+#ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,color=species,shape=species,size=species))
+
+#using alpha for transparency
+#ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,alpha=species))
+
+#assigning a particular color
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,alpha=species),color="purple")
