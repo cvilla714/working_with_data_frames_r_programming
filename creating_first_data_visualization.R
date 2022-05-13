f6c15a8b09444f526e6d736e1e1aa13fc2473bc9
Cosmel VG <cosmelvg@gmail.com>
2022-05-13 15:49:59 +0000

    New content
    New attributes to work with bar charts such as
    - color
    - clarity
    - fill

--- creating_first_data_visualization.R
+++ creating_first_data_visualization.R
@@ -28,4 +28,33 @@ View(penguins)
 #ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,alpha=species))
 
 #assigning a particular color
-ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,alpha=species),color="purple")
+#ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g,alpha=species),color="purple")
+
+#changing from geo_points to geo_smooth
+#ggplot(data = penguins) + geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+#combining geo_smooth and geo_points
+#ggplot(data = penguins) + geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g))+
+ # geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+
+#adding a linetype to represent different species
+ggplot(data = penguins) + geom_smooth(mapping = aes(x = flipper_length_mm, y = body_mass_g,linetype=species))
+
+#adding jitter
+ggplot(data = penguins) + geom_jitter(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+#using diamon dataset for barcharts
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut))
+
+#adding color to the bar chart
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut,color=cut))
+
+#adding color to earch bar
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut,fill=cut))
+
+#addign clarity
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut,fill=clarity))
