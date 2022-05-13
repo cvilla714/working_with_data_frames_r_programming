55394f389de1d9280f1dd7763d086270c932c70f
Cosmel VG <cosmelvg@gmail.com>
2022-05-13 16:36:01 +0000

    New content
    Learn how to insert and manipulate
    - facet_wrap
    - facet_grid

--- creating_first_data_visualization.R
+++ creating_first_data_visualization.R
@@ -58,3 +58,23 @@ ggplot(data=diamonds)+
 #addign clarity
 ggplot(data=diamonds)+
   geom_bar(mapping=aes(x=cut,fill=clarity))
+
+#adding facet_wrap
+ggplot(data=penguins)+
+  geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))+
+  facet_wrap(~species)
+
+#or short way to write it 
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+
+  geom_point(aes(color=species))+
+  facet_wrap(~species)
+
+#adding facet_wrap for diamond data
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=color,fill=cut))+
+  facet_wrap(~cut)
+
+#using facet_grid
+ggplot(data=penguins)+
+  geom_point(aes(x=flipper_length_mm,y=body_mass_g,color=species))+
+  facet_grid(sex~species)
